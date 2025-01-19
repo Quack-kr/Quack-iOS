98f63a483c84d12734255a8bed79b43f9d0945a4
eratchacha <papa021326@naver.com>
2025-01-19 23:48:24 +0900

    feat: SwiftUI TabView

--- Quack/Views/TabBarView.swift
+++ Quack/Views/TabBarView.swift
@@ -9,81 +9,135 @@ import SwiftUI
 
 struct TabBarView: View {
     @State private var tabType : TabContentType = .home;
+    @State private var selection = 0;
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(resource: .raisinBlack)
+    }
     
     var body: some View {
-        ZStack {
-            tabContent(type: tabType)
+        
+        TabView(selection: $selection,
+                content:  {
+            HomeView()
+                .tabItem {
+                    VStack {
+                        Image(.checkCircle) // TODO: 홈이미지 추가
+                            .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                            .frame(width: 24)
+                        
+                        Text("홈")
+                            .font(.pretendard(.number(400)))
+                            .foregroundStyle(Color(hex: "#EFEEDF"))
+                    } }.tag(0)
             
-            VStack {
-                Spacer()
-                
-                HStack {
-                    Button (action: {
-                        tabType = .home;
-                    }) {
-                        VStack {
-                            Image(.checkCircle) // TODO: 홈이미지 추가
-                            .frame(width: 24)
-                            
-                            Text("홈")
-                                .font(.pretendard(.number(400)))
-                                .foregroundStyle(Color(hex: "#EFEEDF"))
-                        }
-                    }
+            SearchView()
+                .tabItem {
+                    Image(.checkCircle) // TODO: 홈이미지 추가
+                        .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                        .frame(width: 24)
                     
-                    Spacer()
+                    Text("홈")
+                        .font(.pretendard(.number(400)))
+                        .foregroundStyle(Color(hex: "#EFEEDF"))
+                }.tag(1)
+            
+            SubstractView()
+                .tabItem {
+                    Image(.checkCircle) // TODO: 홈이미지 추가
+                        .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                        .frame(width: 24)
                     
-                    Button (action: {
-                        tabType = .search;
-                    }) {
-                        VStack {
-                            Image(.checkCircle) // TODO: 검색이미지 추가
-                            .frame(width: 24)
-                            
-                            Text("검색")
-                                .font(.pretendard(.number(400)))
-                                .foregroundStyle(Color(hex: "#EFEEDF"))
-                        }
-                    }
+                    Text("홈")
+                        .font(.pretendard(.number(400)))
+                        .foregroundStyle(Color(hex: "#EFEEDF"))
+                }.tag(2)
+            SubstractView()
+                .tabItem {
+                    Image(.checkCircle) // TODO: 홈이미지 추가
+                        .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                        .frame(width: 24)
                     
-                    Spacer()
-                    
-                    Button (action: {
-                        tabType = .substract;
-                    }) {
-                        VStack {
-                            Image(.checkCircle) // TODO: 빼기 이미지 추가
-                            .frame(width: 24)
-                            
-                            Text("빼기")
-                                .font(.pretendard(.number(400)))
-                                .foregroundStyle(Color(hex: "#EFEEDF"))
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    Button (action: {
-                        tabType = .my;
-                    }) {
-                        VStack {
-                            Image(.checkCircle) // TODO: 마이이미지 추가
-                            .frame(width: 24)
-                            
-                            Text("마이")
-                                .font(.pretendard(.number(400)))
-                                .foregroundStyle(Color(hex: "#EFEEDF"))
-                        }
-                    }
-                
-                }
-                .padding(.top, 8)
-                .padding(.horizontal, 43)
-                .frame(maxWidth: .infinity)
-                .ignoresSafeArea(edges: .bottom)
-                .background(Color(hex:"#21211D"))
-            }
-        }
+                    Text("홈")
+                        .font(.pretendard(.number(400)))
+                        .foregroundStyle(Color(hex: "#EFEEDF"))
+                }.tag(3)
+        })
+        .accentColor(Color(hex:"#EFEEDF"))
+        
+//        ZStack {
+//            tabContent(type: tabType)
+//            
+//            VStack {
+//                Spacer()
+//                
+//                HStack {
+//                    Button (action: {
+//                        tabType = .home;
+//                    }) {
+//                        VStack {
+//                            Image(.checkCircle) // TODO: 홈이미지 추가
+//                                .frame(width: 24)
+//                            
+//                            Text("홈")
+//                                .font(.pretendard(.number(400)))
+//                                .foregroundStyle(Color(hex: "#EFEEDF"))
+//                        }
+//                    }
+//                    
+//                    Spacer()
+//                    
+//                    Button (action: {
+//                        tabType = .search;
+//                    }) {
+//                        VStack {
+//                            Image(.checkCircle) // TODO: 검색이미지 추가
+//                                .frame(width: 24)
+//                            
+//                            Text("검색")
+//                                .font(.pretendard(.number(400)))
+//                                .foregroundStyle(Color(hex: "#EFEEDF"))
+//                        }
+//                    }
+//                    
+//                    Spacer()
+//                    
+//                    Button (action: {
+//                        tabType = .substract;
+//                    }) {
+//                        VStack {
+//                            Image(.checkCircle) // TODO: 빼기 이미지 추가
+//                                .frame(width: 24)
+//                            
+//                            Text("빼기")
+//                                .font(.pretendard(.number(400)))
+//                                .foregroundStyle(Color(hex: "#EFEEDF"))
+//                        }
+//                    }
+//                    
+//                    Spacer()
+//                    
+//                    Button (action: {
+//                        tabType = .my;
+//                    }) {
+//                        VStack {
+//                            Image(.checkCircle) // TODO: 마이이미지 추가
+//                                .frame(width: 24)
+//                            
+//                            Text("마이")
+//                                .font(.pretendard(.number(400)))
+//                                .foregroundStyle(Color(hex: "#EFEEDF"))
+//                        }
+//                    }
+//                    
+//                }
+//                .padding(.top, 8)
+//                .padding(.horizontal, 43)
+//                .frame(maxWidth: .infinity)
+//                .ignoresSafeArea(edges: .bottom)
+//                .background(Color(hex:"#21211D"))
+//            }
+//        }
     }
     
     enum TabContentType {
@@ -107,4 +161,5 @@ struct TabBarView: View {
 
 #Preview {
     TabBarView()
+        .background(Color.background)
 }
