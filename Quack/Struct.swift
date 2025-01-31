adf4ad4d2399d72cf0fc87b83a8118a8f0f2f7d3
eratchacha <papa021326@naver.com>
2025-01-31 22:55:22 +0900

    feat: add searchview initial phase logic

---
+++ Quack/Struct.swift
@@ -0,0 +1,17 @@
+//
+//  Struct.swift
+//  Quack
+//
+//  Created by 차차 on 1/31/25.
+//
+
+import Foundation
+
+struct SearchedShop: Hashable {
+    let shopName: String
+    let img: String = ".dummyMenu"
+    
+    init(shopName: String) {
+        self.shopName = shopName
+    }
+}
